464884fb441cea7f22b1032edb0a86d9116878bf
Sameer Prajapati <sameerprajapati066@gmail.com>
2024-12-19 00:32:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -293,6 +293,6 @@ def logout():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  
+    port = int(os.environ.get("PORT", 8080))  
     app.run(host="0.0.0.0", port=port) 
     # app.run(debug=True)
