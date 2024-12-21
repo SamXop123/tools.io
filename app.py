66b5c3a46c2c1ece82a45878a88a1c0002adaaa8
Sameer Prajapati <sameerprajapati066@gmail.com>
2024-12-21 14:06:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 from gtts import gTTS
 import qrcode
-from PIL import Image
 from io import BytesIO
 import requests
 import random
